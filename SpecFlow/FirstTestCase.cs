810eca6b5110153b5a13ff2e9a4da3debfe55667
hoangnl <nguyenlehoang@hotmail.com>
2016-07-26 07:03:48 +0700

    Init
    Init

---
+++ SpecFlow/FirstTestCase.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecFlow
+{
+    class FirstTestCase
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new OpenQA.Selenium.Firefox.FirefoxDriver();
+            driver.Url = "http://www.demoqa.com";
+        }
+    }
+}
---
+++ SpecFlow/Steps/LogInFeatureSteps.cs
@@ -0,0 +1,57 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowDemo.Steps
+{
+    [Binding]
+    public class LogIn_Steps
+    {
+        public IWebDriver driver;
+        [Given(@"User is at the Home Page")]
+        public void GivenUserIsAtTheHomePage()
+        {
+            driver = new ChromeDriver(@"C:\Selenium\driver");
+            driver.Url = "http://www.store.demoqa.com";
+        }
+
+        [Given(@"Navigate to LogIn Page")]
+        public void GivenNavigateToLogInPage()
+        {
+            driver.FindElement(By.XPath(".//*[@id='account']/a")).Click();
+        }
+
+        [When(@"User enter UserName and Password")]
+        public void WhenUserEnterUserNameAndPassword()
+        {
+            driver.FindElement(By.Id("log")).SendKeys("testuser_1");
+            driver.FindElement(By.Id("pwd")).SendKeys("Test@123");
+        }
+
+        [When(@"Click on the LogIn button")]
+        public void WhenClickOnTheLogInButton()
+        {
+            driver.FindElement(By.Id("login")).Click();
+        }
+
+        [When(@"User LogOut from the Application")]
+        public void WhenUserLogOutFromTheApplication()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Successful LogIN message should display")]
+        public void ThenSuccessfulLogINMessageShouldDisplay()
+        {
+            //This Checks that if the LogOut button is displayed
+            true.Equals(driver.FindElement(By.XPath(".//*[@id='account_logout']/a")).Displayed);
+        }
+
+        [Then(@"Successful LogOut message should display")]
+        public void ThenSuccessfulLogOutMessageShouldDisplay()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
