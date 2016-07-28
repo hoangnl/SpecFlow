978920baeea888740717ff15c6c7ec659a51f7a8
hoangnl <nguyenlehoang@hotmail.com>
2016-07-28 07:06:47 +0700

    Tích hợp SpecFlow+Excel

---
+++ SpecFlow/CalculatorSteps.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using System;
+using TechTalk.SpecFlow;
+
+namespace SpecFlow
+{
+    [Binding]
+    public class CalculatorSteps
+    {
+        private int result { get; set; }
+        private Calculator calculator = new Calculator();
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int number)
+        {
+            calculator.FirstNumber = number;
+        }
+
+        [Given(@"I have also entered (.*) into the calculator")]
+        public void GivenIHaveAlsoEnteredIntoTheCalculator(int number)
+        {
+            calculator.SecondNumber = number;
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            result = calculator.Add();
+        }
+
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int expectedResult)
+        {
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+
+    public class Calculator
+    {
+        public int FirstNumber { set; private get; }
+        public int SecondNumber { set; private get; }
+
+        public int Add()
+        {
+            return FirstNumber + SecondNumber;
+        }
+    }
+}
